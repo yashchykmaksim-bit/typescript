03178df7e2a311d05978d3c6c40db47f30f0ae84
Maksim <yaschyk.maksim@yandex.ru>
2026-07-03 15:57:42 +0300

    Настройка тестов для cart

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -12,3 +12,38 @@ test('cart should no empty', () => {
 
   expect(cart.items.length).toBe(1);
 });
+
+test('totalCost should calculate correct sum of items', () => {
+  const cart = new Cart();
+  cart.add(new Movie(1, 'Movie', 200, 2012, 'USA', 'Нетология отстой', ['Фантастика', 'Боевик'], 122));
+  cart.add(new Movie(1, 'Movie', 200, 2012, 'USA', 'Нетология отстой', ['Фантастика', 'Боевик'], 122));
+  expect(cart.totalCost()).toBe(400);
+});
+
+test('totalCostWithDiscount should calculate correct sum with discount', () => {
+  const cart = new Cart();
+  cart.add(new Movie(1, 'Movie', 200, 2012, 'USA', 'Нетология отстой', ['Фантастика', 'Боевик'], 122));
+  cart.add(new Movie(1, 'Movie', 200, 2012, 'USA', 'Нетология отстой', ['Фантастика', 'Боевик'], 122));
+  const discount = 10;
+  const expectedTotal = 400 * (1 - discount / 100);
+
+  expect(cart.totalCostWithDiscount(discount)).toBe(expectedTotal);
+});
+
+test('removeItemById should remove item correctly', () => {
+  const cart = new Cart();
+  const movie1 = new Movie(101, 'Киллер', 500, 2020, 'USA', 'Боевик', ['Триллер'], 120);
+  const movie2 = new Movie(102, 'Комедия', 300, 2021, 'USA', 'Жанр', ['Комедия'], 100);
+  const movie3 = new Movie(103, 'Ужастик', 400, 2019, 'USA', 'Жанр', ['Ужасы'], 90);
+
+  cart.add(movie1);
+  cart.add(movie2);
+  cart.add(movie3);
+
+  expect(cart.items.length).toBe(3);
+
+  cart.removeItemById(102);
+
+  expect(cart.items.length).toBe(2);
+  expect(cart.items.some(item => item.id === 102)).toBe(false);
+});
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,13 +1,30 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
+  private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  add(item: Buyable): void {
+    this._items.push(item);
+  }
 
-    get items(): Buyable[] {
-        return [...this._items]; 
+  get items(): Buyable[] {
+    return [...this._items];
+  }
+
+  totalCost(): number {
+    let sum: number = 0;
+    for (const item of this._items) {
+      sum += item.price;
     }
+    return sum;
+  }
+  totalCostWithDiscount(discountPercent: number): number {
+    const baseCost = this.totalCost();
+    const discountAmount = baseCost * (discountPercent / 100);
+    const totalWithDiscount = baseCost - discountAmount;
+    return totalWithDiscount;
+  }
+  removeItemById(id: number): void {
+    this._items = this._items.filter((item: Buyable) => item.id !== id);
+  }
 }
